196eee96f4660e707c5851a15372b2f111bc1f3d
Brian Li <brianli1117@gmail.com>
2020-09-22 23:50:10 -0400

    tidy up repo, finalize sprint 0.1

--- web/app.go
+++ web/app.go
@@ -41,7 +41,7 @@ func Serve(app *Application) {
 	// Start http server
 	srv := &http.Server{
 		Handler: 	r,
-		Addr:		"0.0.0.0:8001",
+		Addr:		os.Getenv("CORE_URL"),
 		WriteTimeout: 15 * time.Second,
 		ReadTimeout:  15 * time.Second,
 	}
